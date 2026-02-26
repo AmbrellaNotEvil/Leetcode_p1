a704ed76f2f52ee79b5b8c24de3149c1f4541a49
AmbrellaNotEvil <45904437+AmbrellaNotEvil@users.noreply.github.com>
2026-02-26 03:14:45 +0300

    For task N2 Leetcode

--- Task2.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Sergei_A on 26.02.2026.
-//
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-class Solution {
+class Solution1 {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
@@ -18,4 +18,33 @@ public:
         return result;
     }
 };
+class Solution2 {
+public:
+    bool isPalindrome(int x) {
+        vector <int> reverses;
+        vector <int> primary;
+        bool result;
+        while (x > 0)
+        {
+            int digit = x%10;
+            x /= 10;
+            primary.push_back(digit);
+            reverses.push_back(digit);
+        }
+        reverse(reverses.begin(), reverses.end());
+        if (reverses ==  primary) {
+            result = true;
+        }
+        if (reverses !=  primary) {
+            result = false;
+        }
+        if (x < 0) {
+            result = false;
+        }
+        return result;
+    }
+};
 
+int main() {
+
+};
